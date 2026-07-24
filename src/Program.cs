3540a1b82c598ed39be3b3ae2408d62c225b94b2
kkhkl <141620334+kkhkl@users.noreply.github.com>
2026-07-25 01:47:24 +0800

    Create Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+namespace KLHash;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
